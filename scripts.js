9471d1d2dda5f14bebc146c99637906830672ea1
alternativy <alternativy4.0@gmail.com>
2023-11-19 13:16:52 -0300

    arreglé el enlace de trabajos

--- scripts.js
+++ scripts.js
@@ -1,42 +1,73 @@
- const enlaces = document.querySelectorAll('nav a');
+//  const enlaces = document.querySelectorAll('nav a');
 
-  // Recorrer los enlaces y agregar un evento "click" a cada uno
-  enlaces.forEach((enlace) => {
-    enlace.addEventListener('click', (evento) => {
-      // Prevenir el comportamiento por defecto del enlace (scroll)
-      evento.preventDefault();
+//   // Recorrer los enlaces y agregar un evento "click" a cada uno
+//   enlaces.forEach((enlace) => {
+//     enlace.addEventListener('click', (evento) => {
+//       // Prevenir el comportamiento por defecto del enlace (scroll)
+//       evento.preventDefault();
 
-      // Obtener el ID de la sección correspondiente al enlace
-      const seccionId = enlace.getAttribute('href').substring(1);
+//       // Obtener el ID de la sección correspondiente al enlace
+//       const seccionId = enlace.getAttribute('href').substring(1);
 
-      // Obtener todas las secciones
-      const secciones = document.querySelectorAll('main > div');
+//       // Obtener todas las secciones
+//       const secciones = document.querySelectorAll('main > div');
 
-      // Recorrer las secciones y ocultar todas excepto la correspondiente al enlace
-      secciones.forEach((seccion) => {
-        if (seccion.getAttribute('id') === seccionId) {
-          seccion.classList.remove('oculto');
-        } else {
-          seccion.classList.add('oculto');
-        }
-      });
+//       // Recorrer las secciones y ocultar todas excepto la correspondiente al enlace
+//       secciones.forEach((seccion) => {
+//         if (seccion.getAttribute('id') === seccionId) {
+//           seccion.classList.remove('oculto');
+//         } else {
+//           seccion.classList.add('oculto');
+//         }
+//       });
+//     });
+//   });
+
+//   // Obtener todos los elementos del menú
+//   let navLinks = document.querySelectorAll("nav a");
+
+//   // Iterar sobre cada elemento y agregar un listener de clic
+//   navLinks.forEach(function(link) {
+//     link.addEventListener("click", function(event) {
+//       // Prevenir que el enlace redirija a otra página
+//       event.preventDefault();
+
+//       // Quitar la clase "selected" del elemento anterior
+//       let previousLink = document.querySelector("nav a.selected");
+//       previousLink.classList.remove("selected");
+
+//       // Agregar la clase "selected" al elemento clickeado
+//       link.classList.add("selected");
+//     });
+//   });
+
+const enlaces = document.querySelectorAll('nav a');
+
+enlaces.forEach((enlace) => {
+  enlace.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    
+    const seccionId = enlace.getAttribute('href').substring(1);
+    const secciones = document.querySelectorAll('main > div');
+
+    secciones.forEach((seccion) => {
+      if (seccion.getAttribute('id') === seccionId) {
+        seccion.classList.remove('oculto');
+      } else {
+        seccion.classList.add('oculto');
+      }
     });
-  });
 
-  // Obtener todos los elementos del menú
-  let navLinks = document.querySelectorAll("nav a");
+    // Caso especial para "Trabajos": quitar la clase "oculto" de la sección "audiovisuales"
+    if (seccionId === 'trabajos') {
+      const audiovisualesSection = document.querySelector('.audiovisuales');
+      audiovisualesSection.classList.remove('oculto');
+    }
 
-  // Iterar sobre cada elemento y agregar un listener de clic
-  navLinks.forEach(function(link) {
-    link.addEventListener("click", function(event) {
-      // Prevenir que el enlace redirija a otra página
-      event.preventDefault();
-
-      // Quitar la clase "selected" del elemento anterior
-      let previousLink = document.querySelector("nav a.selected");
-      previousLink.classList.remove("selected");
-
-      // Agregar la clase "selected" al elemento clickeado
-      link.classList.add("selected");
+    enlaces.forEach((link) => {
+      link.classList.remove('selected');
     });
+
+    enlace.classList.add('selected');
   });
+});
